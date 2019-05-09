d66ac1af19047393559b9d1a351d7d8b8a96b407
CeciliaNi <aiqniyuan@163.com>
2019-05-09 10:43:34 +0800

    committest

--- run_entrance/unittest_runner.py
+++ run_entrance/unittest_runner.py
@@ -13,7 +13,7 @@ report_path = '../report/'
 case_path = '../run_entrance/'
 
 
-def add_case(casepath=case_path, rule="test_api_login.py"):
+def add_case(casepath=case_path, rule="test_api_banli.py"):
     """
     加载所有的测试文件
     :return:
