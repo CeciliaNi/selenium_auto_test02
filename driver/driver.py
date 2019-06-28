f20f0717f0ee5f2ed36adb276243070fb851b1ae
CeciliaNi <aiqniyuan@163.com>
2019-06-28 16:42:07 +0800

    commit by niyuan@2019/6/28

--- driver/driver.py
+++ driver/driver.py
@@ -5,15 +5,23 @@
 """
 
 from selenium import webdriver
-
+import os
 
 # 无界面运行模式
-# chrome_options = webdriver.ChromeOptions()
+chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument('--headless')
 # chrome_options.add_argument('--disable-gpu')
 # driver = webdriver.Chrome(chrome_options=chrome_options)
 
+default_directory_rel = '../run_entrance/down_file/'
+default_directory_abs = os.path.abspath(default_directory_rel)
+
+prefs = {'download.default_directory': default_directory_abs}
+chrome_options.add_experimental_option('prefs', prefs)
+
+
 # 初始化driver
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
+driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.maximize_window()
 driver.implicitly_wait(40)   # 隐性等待，最长等20秒
